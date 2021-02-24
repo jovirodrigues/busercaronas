18dfbe3ace5cefe864c50bbbfb12e27e0b98ce54
joao.rodrigues <joao.rodrigues@buser.com.br>
2021-02-24 13:55:55 -0300

    Apaga todas as paginas e componentes não usados

--- frontend/layouts/complex.vue
+++ frontend/layouts/complex.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
     <toolbar :state="layout"/>
-    <sidenav-right :state="layout"/>
-    <sidenav-left :state="layout"/>
-    <sidenav-left-temp :state="layout"/>
-    <sidenav-right-temp :state="layout"/>
     <v-content>
       <v-container fluid>
         <nuxt></nuxt>
@@ -16,18 +12,10 @@
 
 <script>
   import toolbar from '~/components/toolbar.vue'
-  import sidenavLeft from '~/components/sidenav-left.vue'
-  import sidenavRight from '~/components/sidenav-right.vue'
-  import sidenavLeftTemp from '~/components/sidenav-left-temp.vue'
-  import sidenavRightTemp from '~/components/sidenav-right-temp.vue'
   import footer from '~/components/footer.vue'
   export default {
     components: {
       toolbar,
-      sidenavLeft,
-      sidenavRight,
-      sidenavLeftTemp,
-      sidenavRightTemp,
       leFooter: footer
     },
     data: () => ({
